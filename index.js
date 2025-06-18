0024e5481bd653a47246b16104313d3fea23f402
Mohammed Sameh <ms122g@gmail.com>
2025-06-18 21:14:54 +0300

    V1 Without Responsive

---
+++ index.js
@@ -0,0 +1,14 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 30,
+  nav: false,
+  dots: true,
+  autoplay: true,
+  responsive: {
+    0: { items: 1 },
+    768: { items: 2 },
+    1024: { items: 3 }
+  }
+});
+
+
