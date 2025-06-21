57c407e332e8711579b85fd93a96a35a3628ed42
Mohammed Sameh <ms122g@gmail.com>
2025-06-22 02:25:20 +0300

    V5 Page1 Edited and Responsive Done

--- index.js
+++ index.js
@@ -10,5 +10,30 @@ $(".owl-carousel").owlCarousel({
     1024: { items: 3 }
   }
 });
+  const links = document.querySelectorAll('.nav-link');
+  // خزّن الألوان الأصلية لكل زر
+  links.forEach(link => {
+    link.setAttribute('data-color', link.style.color);
+  });
+  function setActiveLink(clickedLink) {
+    links.forEach(link => {
+      link.classList.remove('active');
+      link.style.color = link.getAttribute('data-color');
+    });
+
+    clickedLink.classList.add('active');
+    clickedLink.style.color = '#DA9F5B'; // ← اللون اللي انت عايزه
+  }
+  // أول ما الصفحة تفتح، فعل لينك الـ Home
+  window.addEventListener('DOMContentLoaded', () => {
+    const homeLink = document.getElementById('home-link');
+    setActiveLink(homeLink);
+  });
+  // لما المستخدم يضغط على أي زر
+  links.forEach(link => {
+    link.addEventListener('click', function () {
+      setActiveLink(this);
+    });
+  });
 
 
