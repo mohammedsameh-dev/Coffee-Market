7110fd93d953e769ffe2ce95c757f6b3447a0588
Mohammed Sameh <ms122g@gmail.com>
2025-06-22 21:04:03 +0300

    V6 Adding Animations and Responsive

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ $(".owl-carousel").owlCarousel({
     1024: { items: 3 }
   }
 });
+
+  const myCarousel = new bootstrap.Carousel('#carouselExampleRide', {
+    interval: 3000,
+    ride: 'carousel'
+  });
+
   const links = document.querySelectorAll('.nav-link');
   // خزّن الألوان الأصلية لكل زر
   links.forEach(link => {
@@ -35,5 +41,27 @@ $(".owl-carousel").owlCarousel({
       setActiveLink(this);
     });
   });
+  
+  // اعمل انيميشن لما اسكرول ياض
 
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const element = entry.target;
+        const animationClass = element.dataset.animate;
 
+        element.classList.add('visible'); // يخليها تطلع بسلاسة
+        element.classList.add('animate__animated', animationClass);
+
+        observer.unobserve(element); // يشغل مرة واحدة بس
+      }
+    });
+  }, {
+    threshold: 0.2
+  });
+
+  document.querySelectorAll('.scroll-animate').forEach(el => {
+    observer.observe(el);
+  });
+
+  
